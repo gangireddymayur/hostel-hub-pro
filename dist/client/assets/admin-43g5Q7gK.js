1c7b5c281d209bab1daeabc315422f919053724c
Mayur <mayurgangereddy12345@gmail.com>
2026-06-20 20:01:56 +0530

    build: refresh and update hashed assets in distribution directory

--- dist/client/assets/admin-43g5Q7gK.js
+++
@@ -1,1 +0,0 @@
-import{j as o}from"./index-BdwFm29H.js";import{D as t}from"./dashboard-shell-BhgffA11.js";import"./index-OUW9kdl1.js";const r=()=>o.jsx(t,{expectedRole:"admin"});export{r as component};
---
+++ dist/client/assets/super-vjVpiPWo.js
@@ -0,0 +1,1 @@
+import{j as o}from"./index-qVEyqQWA.js";import{D as t}from"./dashboard-shell-D6f1u6vh.js";import"./index-BHURV2Uw.js";const s=()=>o.jsx(t,{expectedRole:"super"});export{s as component};
